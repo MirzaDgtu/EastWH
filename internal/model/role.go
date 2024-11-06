724794dca397e774574b87817e4e21255e039a62
Mirza Ali <mirzyali@mail.ru>
2024-11-06 22:05:51 +0300

    add folder log

--- internal/model/role.go
+++ internal/model/role.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Role struct {
 	gorm.Model
-	Name        string `json:"name"`
+	Name        string `gorm:"unique" json:"name"`
 	Description string `json:"description" validation:"required"`
 	Priority    int    `json:"priority"`
 	Users       []User `gorm:"many2many:user_roles;" json:"users"`
