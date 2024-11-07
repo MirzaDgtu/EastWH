8a9bc84dab47af3c2df148f1d1876fd8209e0a2f
sashashdln <sashashdln@vk.com>
2024-11-07 13:03:57 +0700

    add userProject

--- internal/store/sqlstore/userProjectRepository.go
+++ internal/store/sqlstore/userProjectRepository.go
@@ -11,12 +11,12 @@ func (r *UserProjectRepository) Add(u model.UserProject) (model.UserProject, err
 }
 
 func (r *UserProjectRepository) Update(userproject model.UserProject) (model.UserProject, error) {
-	return userproject, r.store.db.Save(&userproject).Error
+	return userproject, r.store.db.Model(&userproject).Update("user_id", userproject.UserID).Update("project_id", userproject.ProjectID).Error
 }
 
 func (r *UserProjectRepository) Delete(id uint) error {
 	var up model.UserProject
-	result := r.store.db.Table("userprojects").Where("id=,", id)
+	result := r.store.db.Table("user_projects").Where("id=?", id)
 	err := result.First(&up)
 	if err != nil {
 		return nil
@@ -24,16 +24,17 @@ func (r *UserProjectRepository) Delete(id uint) error {
 	return r.store.db.Delete(&up).Error
 }
 
-func (r *UserProjectRepository) ByID(ID uint) (up model.UserProject, err error) {
-	return up, r.store.db.First(&up, ID).Error
+func (r *UserProjectRepository) ByID(Id uint) (up model.UserProject, err error) {
+	up.ID = Id
+	return up, r.store.db.First(&up, Id).Error
 }
 
 func (r *UserProjectRepository) ByUserID(userID uint) (userproject []model.UserProject, err error) {
-	return userproject, r.store.db.Where("user_id").Find(&userproject).Error
+	return userproject, r.store.db.Where("user_id=?", userID).Find(&userproject).Error
 }
 
 func (r *UserProjectRepository) ByProjectID(projectID uint) (userproject []model.UserProject, err error) {
-	return userproject, r.store.db.Where("project_id", projectID).Find(&userproject).Error
+	return userproject, r.store.db.Where("project_id=?", projectID).Find(&userproject).Error
 }
 
 func (r *UserProjectRepository) All() (userproject []model.UserProject, err error) {
